25aa8a5fb3443d29e1aa2d1bfb55666bc657ea2b
rehan <Technosis141@gmail.com>
2023-09-07 15:44:36 -0400

    fixing bugs - playlist size increased

--- app.js
+++ app.js
@@ -1,10 +1,79 @@
 require("dotenv").config();
+
 const express = require("express");
 const expressLayouts = require("express-ejs-layouts");
 const path = require("path");
 const SpotifyWebApi = require("spotify-web-api-node");
-
+const axios = require('axios');
+const querystring = require('querystring');
 const app = express();
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => {
+  console.log('Connected to MongoDB');
+})
+.catch((err) => {
+  console.error('Error connecting to MongoDB:', err);
+});
+
+let redirect_uri = 
+  process.env.REDIRECT_URI || 
+  'http://localhost:3000/callback';
+
+app.get('/login', (req, res) => {
+    res.redirect('https://accounts.spotify.com/authorize?' +
+      querystring.stringify({
+        response_type: 'code',
+        client_id: process.env.CLIENT_ID,
+        scope: 'user-read-private user-read-email',
+        redirect_uri
+      }));
+});
+
+app.get('/callback', (req, res) => {
+    let code = req.query.code || null;
+
+    let authOptions = {
+      url: 'https://accounts.spotify.com/api/token',
+      form: {
+        code: code,
+        redirect_uri,
+        grant_type: 'authorization_code'
+      },
+      headers: {
+        'Authorization': 'Basic ' + (new Buffer.from(
+          process.env.CLIENT_ID + ':' + process.env.CLIENT_SECRET
+        ).toString('base64')),
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      json: true
+    };
+
+    axios({
+      method: 'post',
+      url: authOptions.url,
+      data: querystring.stringify(authOptions.form),
+      headers: authOptions.headers
+    }).then(response => {
+      var access_token = response.data.access_token;
+      var refresh_token = response.data.refresh_token;
+
+      return axios({
+        method: 'get',
+        url: 'https://api.spotify.com/v1/me',
+        headers: { 'Authorization': 'Bearer ' + access_token }
+      });
+    }).then(response => {
+      console.log(response.data);
+      res.redirect('/your-redirect-url');
+    }).catch(error => {
+      console.error('Error:', error);
+    });
+});
 
 // Set up middleware
 app.use(expressLayouts);
@@ -54,31 +123,33 @@ app.get("/", (req, res) => {
 });
 
 app.get("/artist-search", async (req, res) => {
-  const artistSearch = req.query["artist-search"];
-
-  if (!artistSearch) {
-    res.send("No search provided");
-    return;
-  }
-
-  try {
-    const data = await getArtist(artistSearch);
-    const artist = data.body.artists.items;
-    const pageTitle = artist[0].name;
-    const pageStyling = "/styles/artist-style.css";
-    res.render("artist-search-results", {
-      artist: artist[0],
-      pageTitle,
-      pageStyling,
-    });
-  } catch (error) {
-    console.log("There has been an error:", error);
-    res.render("error");
-  }
-});
+    const artistSearch = req.query["artist-search"];
+  
+    if (!artistSearch) {
+      res.send("No search provided");
+      return;
+    }
+  
+    try {
+      const data = await getArtist(artistSearch);
+      const artists = data.body.artists.items;
+      const pageTitle = "Search Results";
+      const pageStyling = "/styles/artist-style.css";
+      res.render("artist-search-results", {
+        artists,
+        pageTitle,
+        pageStyling,
+      });
+    } catch (error) {
+      console.log("There has been an error:", error);
+      res.render("error");
+    }
+  });
+  
 
 app.get("/all-albums/:artistId", async (req, res) => {
   const artistId = req.params.artistId;
+  console.log("Artist ID:", artistId);
   const pageTitle = "Albums";
   const pageStyling = "/styles/album-style.css";
 
@@ -111,6 +182,5 @@ app.listen(port, () =>
   console.log(`Betterify Running on ${port} 🎧`)
 );
 
-// return multiple results 
-// multiple artists 
-// return 
+
+// 
