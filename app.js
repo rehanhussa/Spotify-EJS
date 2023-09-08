97cd50eaf44641f0e8bb70d98cf47c474eb32385
rehan <Technosis141@gmail.com>
2023-09-08 11:54:05 -0400

    update for deploy

--- app.js
+++ app.js
@@ -263,10 +263,7 @@ app.post('/save-favorite-artist', async (req, res) => {
 
 
 // Start the server
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () =>
   console.log(`Betterify Running on ${port} 🎧`)
 );
-
-
-// 
